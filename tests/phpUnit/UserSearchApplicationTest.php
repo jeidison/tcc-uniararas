f11b35db94171f1dc53a318467ed0745954baa6b
Jeidison Farias <jeidison.farias@gmail.com>
2017-09-02 23:53:43 +0000

    Ajustes no nome de classes para os testes em PHPUnit + Teste para o search, delete, update para PHPUnit.

---
+++ tests/phpUnit/UserSearchApplicationTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * User: jeidison
+ * Date: 24/08/17
+ * Time: 20:09
+ */
+class UserSearchApplicationTest extends TestCase
+{
+
+    public function testSearchWithAllParams()
+    {
+        $searchParam = new SearchParam();
+        $searchParam->dhNasc = "2000-01-01";
+        $searchParam->document = "999999999";
+        $appUser = new UserApplication();
+        $result = $appUser->search($searchParam);
+        $this->assertNotNull($result);
+    }
+
+    public function testSearchWithDhNasc()
+    {
+        $searchParam = new SearchParam();
+        $searchParam->dhNasc = "2000-01-01";
+        $appUser = new UserApplication();
+        $result = $appUser->search($searchParam);
+        $this->assertNotNull($result);
+    }
+
+    public function testSearchWithDocument()
+    {
+        $searchParam = new SearchParam();
+        $searchParam->document = "999999999";
+        $appUser = new UserApplication();
+        $result = $appUser->search($searchParam);
+        $this->assertNotNull($result);
+    }
+
+    public function testSearchWithoutParam()
+    {
+        $searchParam = new SearchParam();
+        $appUser = new UserApplication();
+        $result = $appUser->search($searchParam);
+        $this->assertNotNull($result);
+    }
+
+    public function testSearchWithParamsNull()
+    {
+        $appUser = new UserApplication();
+        $result = $appUser->search(null);
+        $this->assertNotNull($result);
+    }
+
+}
