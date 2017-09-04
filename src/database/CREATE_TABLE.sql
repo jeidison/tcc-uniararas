3e446456926f5737eed70b446b99d6302bdb3caa
Paulo Cruz <paulo.cruz@outlook.com>
2017-09-04 21:49:46 +0000

    Arquivos database ...

---
+++ src/database/CREATE_TABLE.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user" (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	document VARCHAR(11) UNIQUE NOT NULL,
+	sex VARCHAR(1) NOT NULL CHECK (sex = 'F' or sex = 'M'),
+	date_birth DATE NOT NULL,
+	phone VARCHAR(11),
+	email VARCHAR,
+	status VARCHAR(7) NOT NULL CHECK (status = 'ATIVO' or status = 'INATIVO')
+);
---
+++ src/database/INSERT_VALUES.sql
@@ -0,0 +1,14 @@
+INSERT INTO "user"(name,
+                   document,
+                   sex,
+                   date_birth,
+                   phone,
+                   email,
+                   status)
+VALUES ('AAAA AAAA AAAA AAAA',
+        '22222222222',
+        'M',
+        '10/01/2009',
+        '45465465464',
+        'teste@teste.com',
+        'ATIVO');
