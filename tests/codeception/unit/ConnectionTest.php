54b5be9a13e996e54122b695ed2313ab69c81e82
Paulo Cruz <paulo.cruz@tstechbrasil.com.br>
2017-09-13 11:45:32 -0300

    Teste DAO Connection...

--- tests/codeception/unit/ConnectionTest.php
+++ tests/codeception/unit/ConnectionTest.php
@@ -1,20 +1,16 @@
 <?php
 
-
-require __DIR__.'/../../../src/model/dao/Connection.php';
+require __DIR__.'/../../../src/model/dao/UserDao.php';
 
 class ConnectionTest extends \Codeception\Test\Unit
 {
-    use Connection as ConnectionDB;
+    use Connection;
 
     public function testConnectionWithInvalidPassword()
     {
-      /*
         $this->setDbpass('invalidaPassword');
         $connection = $this->connection();
         $this->assertNotNull($connection);
         $this->assertTrue(is_string($connecton));
-      */
     }
-
 }
