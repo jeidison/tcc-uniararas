7a724cd5e8463202a24f6a3237e6ecbd4db6a2e9
Paulo Cruz <paulo.cruz@tstechbrasil.com.br>
2017-09-13 10:36:23 -0300

    Ajustes...

--- tests/codeception/unit/ConnectionTest.php
+++ tests/codeception/unit/ConnectionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-//require __DIR__.'/../../../src/model/dao/Connection.php';
+require __DIR__.'/../../../src/model/dao/Connection.php';
 
 class ConnectionTest extends \Codeception\Test\Unit
 {
@@ -8,10 +8,10 @@ class ConnectionTest extends \Codeception\Test\Unit
 
     public function testConnectionWithInvalidPassword()
     {
-        /*$this->setDbpass('invalidaPassword');
+        $this->setDbpass('invalidaPassword');
         $connection = $this->connection();
         $this->assertNotNull($connection);
-        $this->assertTrue(is_string($connecton));*/
+        $this->assertTrue(is_string($connecton));
     }
 
 }
